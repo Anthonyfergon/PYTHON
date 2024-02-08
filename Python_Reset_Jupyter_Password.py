66cb540f1f2120ae1ebd153d1426a0e2754eeec1
Anthony Fernandez Gonzalez <59268223+Anthonyfergon@users.noreply.github.com>
2024-02-08 08:11:02 -0600

    Add files via upload

---
+++ Python_Reset_Jupyter_Password.py
@@ -0,0 +1,21 @@
+# _____________________________________________________________________
+#                            PYTHON_GOSPEL
+#                         DOGMA & DOCTRINES
+#               Prepared by AnthonyCRENG28, Rabbit industries.      
+# _____________________________________________________________________
+
+#### 01. KEY_CONCEPTS
+
+#### 01.01 JUPYTER_ENV_THE KERNEL_NOTEBOOKS
+# https://jupyter-notebook-beginner-guide.readthedocs.io/en/latest/what_is_jupyter.html#what-is-the-jupyter-notebook
+# https://jupyter.org/
+
+#### 02. RESETTING JUPYTER PASSWORD
+01. Go to Windows/Search and select: Anaconda Prompt(Anaconda 3)
+02. Go to: (base) C:\Users\Asus VivoBook>
+03. Execute the following command: jupyter notebook password
+04. update your password
+05. done. Now type: jupyter notebook
+06. Enter your new password.
+07. FINISH
+# https://usdrcg.gitbook.io/docs/lawrence-hpc/jupyter-on-lawrence
